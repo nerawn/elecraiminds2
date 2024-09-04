5c11cef061a1fd328b683bc33e54fad47fde1461
Hakan <celikhakan5255@gmail.com>
2024-09-04 15:31:43 +0000

    fix: merge

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
 	output: 'export'
-=======
-  output: "export",
->>>>>>> b89213c7645cf5654eca83cb09dfd83e228a833d
 };
 
 export default nextConfig;
